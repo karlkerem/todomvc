cf5be9665a9f83251153abd0ebc33a51b223e5ad
Kerem, Karl <karl@prismfp.com>
2018-02-27 12:19:18 +0000

    Add prmService to the project

---
+++ examples/angularjs/js/services/prmService.js
@@ -0,0 +1,29 @@
+
+
+angular.module('todomvc')
+
+
+.factory('prmService', function ($q, $timeout) {
+
+  return {
+
+    /**
+     * Checks if proposed todo title contains keywords that warrants
+     * a prompt from user to confirm they  
+     */
+    isPrivacyConfirmNeeded: function(proposedTodoTitle) {
+      var deferred = $q.defer();
+      if (proposedTodoTitle.toLowerCase().indexOf('prism')) {
+        var delay = Math.floor(Math.random()*900+100);
+        $timeout(function() {
+          deferred.resolve(true);
+        }, delay);
+      }
+      else {
+        deferred.resolve(false);
+      }
+      return deferred.promise;
+    }
+  }
+
+});
